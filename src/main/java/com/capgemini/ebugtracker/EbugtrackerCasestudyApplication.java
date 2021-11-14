57c57fccb08bff9312d88ea507214ac2598b4b94
vijayalaxmi8 <89607942+vijayalaxmi8@users.noreply.github.com>
2021-11-14 18:13:24 +0530

    update

--- src/main/java/com/capgemini/ebugtracker/EbugtrackerCasestudyApplication.java
+++ src/main/java/com/capgemini/ebugtracker/EbugtrackerCasestudyApplication.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+//@ComponentScan(basePackages = {"com.capgemini.ebugtracker.admin.controller","com.capgemini.ebugtracker.admin.entity","com.capgemini.ebugtracker.admin.repositery","com.capgemini.ebugtracker.admin.service","com.capgemini.ebugtracker.admin.service.AdminServices","com.capgemini.ebugtracker.staff","com.capgemini.ebugtracker.user", "com.capgemini.ebugtracker.mailsender"})
 @ComponentScan("com.capgemini.ebugtracker.admin.service.AdminServices")
 @EnableJpaRepositories("com.capgemini.ebugtracker.staff.repositery.StaffDao")
 public class EbugtrackerCasestudyApplication {
