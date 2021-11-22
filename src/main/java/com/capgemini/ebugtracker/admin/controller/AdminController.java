661c070fe8e2664043f59a77d497d233d3d9d5dc
AKASH KASAR <akash.kasar20798@gmail.com>
2021-11-22 13:48:55 +0530

    Commit

--- src/main/java/com/capgemini/ebugtracker/admin/controller/AdminController.java
+++ src/main/java/com/capgemini/ebugtracker/admin/controller/AdminController.java
@@ -7,6 +7,7 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -29,6 +30,7 @@ import javax.mail.MessagingException;
  * @author v62
  *
  */
+@CrossOrigin(origins="http://localhost:4200")
 @RestController
 @RequestMapping("/admin")
 public class AdminController {
